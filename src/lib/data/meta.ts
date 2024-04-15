66ef5aefe30e7b21830d85137bc2dc5bcf1683ec
Lim Yi Sheng Brian <83122861+BrianLYS@users.noreply.github.com>
2024-04-15 13:04:13 +0800

    chore(meta.ts): remove boilerplate

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -7,15 +7,38 @@ export const siteBaseUrl = 'https://idkeverything.com/';
 export const keywords = [
 	'Svelte',
 	'SvelteKit',
-	'Template',
 	'Blog',
-	'Starter',
-	'Static Site'
+	'Static Site',
+	'Idkeverything',
+	'Idk',
+	'Everything',
+	'Artificial Intelligence',
+	'AI',
+	'Engineering',
+	'Programming',
+	'Biotechnology',
+	'Bioinformatics',
+	'Biomedical Engineering',
+	'Biomedical Science',
+	'Research',
+	'Innovation',
+	'Renewable Energy',
+	'Climate Change',
+	'Environmental Science',
+	'Sustainability',
+	'Machine Learning',
+	'Data Science',
+	'Cybersecurity',
+	'Web Development',
+	'Web Design',
+	'Web Hosting',
+	'Knowledge',
+	'Cloud Computing',
 ];
 
 export const description =
-	"A light, neat, and easy-to-use SvelteKit template for your next website.";
+	"Idkeverything. A blog about everything I don't know, but I know will have huge implications for the future. From artificial intelligence to renewable energy, I'm here to learn and share.";
 
-export const title = 'SvelteKit Static Blog Template';
+export const title = 'Idkeverything';
 
 export const image = `${siteBaseUrl}/images/site-preview.png`;
